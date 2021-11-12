44d368100219714281d65c67b8d31852a145f6f9
Ivan Makarevich <i.makarevich@multiplex.ua>
2021-11-13 00:04:08 +0200

    Push ticket name to firebase

--- lib/screens/ticket/tickets_screen.dart
+++ lib/screens/ticket/tickets_screen.dart
@@ -5,12 +5,68 @@ import 'package:provider/provider.dart';
 import '../dashboard/components/header.dart';
 import 'package:admin/constants.dart';
 import '../main/components/side_menu.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class TicketsScreen extends StatelessWidget {
+class TicketsScreen extends StatefulWidget {
+  @override
+  State<TicketsScreen> createState() => _TicketsScreenState();
+}
+
+class _TicketsScreenState extends State<TicketsScreen> {
   final String _pageName = 'Ticket system';
+  bool _initialized = false;
+  bool _error = false;
+  String ticketName = '';
+  late CollectionReference tickets;
+  // final Stream<QuerySnapshot> tickets;
+  // FirebaseFirestore.instance.collection('users').snapshots();
+
+  @override
+  void initState() {
+    WidgetsFlutterBinding.ensureInitialized();
+    initializeFlutterFire();
+    tickets = FirebaseFirestore.instance.collection('tickets');
+    super.initState();
+  }
+
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  void addName({required String ticketName}) {
+    tickets
+      .doc('qmtLQpFLneoVpBq9FzMG')
+      .set({
+        'name': ticketName,
+      })
+      .then((value) => print("Name is send to firebase"))
+      .catchError((error) => print("Failed send name to firebase: $error"));
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (_error) {
+      print('SomethingWentWrong');
+    }
+
+    // Show a loader until FlutterFire is initialized
+    if (!_initialized) {
+      print('Loading');
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Header(),
@@ -20,47 +76,44 @@ class TicketsScreen extends StatelessWidget {
       ),
       key: context.read<MenuController>().scaffoldKey,
       drawer: SideMenu(),
-      body: SafeArea(
-        // child: Row(
-        //   crossAxisAlignment: CrossAxisAlignment.start,
-        //   children: [
-        //     Expanded(
-        //       // It takes 5/6 part of the screen
-        //       flex: 6,
-        //
-        //       child: Column(
-        //         mainAxisAlignment: MainAxisAlignment.center,
-        //         children: <Widget>[
-        //           Text(
-        //             'You pressed menu: ',
-        //           ),
-        //           Text(
-        //             '$_pageName',
-        //             style: TextStyle(
-        //               fontWeight: FontWeight.bold,
-        //               fontSize: 40,
-        //               // decorationStyle: TextDecorationStyle.wavy,
-        //               foreground: Paint()
-        //                 ..style = PaintingStyle.stroke
-        //                 ..color = Colors.blue[100]!,
-        //             ),
-        //           ),
-        //         ],
-        //       ),
-        //     ),
-        //   ],
-        // ),
+      body: SafeArea(       
         child: Column(
           children: [
-            ElevatedButton(onPressed: ()=> {
+          //   Container(
+          //     padding: EdgeInsets.only(top: 30),
+          //     child: Text('Plugin'),
+          //   ),
+            Padding(padding: const EdgeInsets.only(top: 30)),
 
-            }, child: Text('add new'),
-
-                style: ButtonStyle(
-                  // padding: EdgeInsets.only(top: 10.0);
-                ),
-                    ),
-            Padding(padding: EdgeInsets.all(20),
+            ElevatedButton(
+              onPressed: () => {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Добавить'),
+                        content: TextField(onChanged: (String value) {
+                          ticketName = value;
+                        }),
+                        actions: [
+                          ElevatedButton(
+                            // onPressed: addName(ticketName: ''),
+                            onPressed: () {
+                              addName(ticketName: ticketName);
+                            },
+                            child: Text('Добавить'),
+                          )
+                        ],
+                      );
+                    })
+              },
+              child: Text('add new'),
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all(const EdgeInsets.all(20)),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
               child: Card(
                 child: InkWell(
                   splashColor: Colors.blue.withAlpha(30),
@@ -72,7 +125,7 @@ class TicketsScreen extends StatelessWidget {
                     height: 100,
                     child: Text('A card that can be tapped'),
                   ),
-                  ),
+                ),
               ),
             ),
           ],
