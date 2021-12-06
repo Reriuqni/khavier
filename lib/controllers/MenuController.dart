c95b43d8ae0e3f1286ba835b0dc4ec3785208e13
Ivan Makarevich <i.makarevich@multiplex.ua>
2021-12-06 10:38:19 +0200

    wip

--- lib/controllers/MenuController.dart
+++ lib/controllers/MenuController.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class MenuController extends ChangeNotifier {
-  final GlobalKey<ScaffoldState> _scaffoldKeyMenuController = GlobalKey<ScaffoldState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
-  GlobalKey<ScaffoldState> get scaffoldKey => _scaffoldKeyMenuController;
+  GlobalKey<ScaffoldState> get scaffoldKey => _scaffoldKey;
 
   void controlMenu() {
-    if (!_scaffoldKeyMenuController.currentState.isDrawerOpen) {
-      _scaffoldKeyMenuController.currentState.openDrawer();
+    if (!_scaffoldKey.currentState.isDrawerOpen) {
+      _scaffoldKey.currentState.openDrawer();
     }
   }
 }
--- lib/screens/dashboard/components/header.dart
+++ lib/screens/dashboard/components/header.dart
@@ -31,123 +31,127 @@ class _HeaderState extends State<Header> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment:  CrossAxisAlignment.start,
-          children: [
-            Container(
-      margin: EdgeInsets.all(0),
-                padding: EdgeInsets.fromLTRB(100, 15, 100, 15),
-                color: Color(0xB2000000),
-                child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        // if (!Responsive.isDesktop(context))
-        //   IconButton(
-        //     icon: Icon(Icons.menu),
-        //     onPressed: context.read<MenuController>().controlMenu,
-        //   ),
-                    // if (!Responsive.isMobile(context))
-                    //   Spacer(flex: Responsive.isDesktop(context) ? 2 : 1),
-                    // Expanded(child: SearchField()),
-                    // ProfileCard()
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        TextButton(
-                          onPressed: () {},
-                          style: ButtonStyle(
-                              overlayColor: MaterialStateProperty.all(secondaryColor)
-                          ),
-                          child: SvgPicture.asset('assets/icons/exclamation-square.svg', width: 20, height: 20, color: primaryColor),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            setState(() {
-                              settingsShow = settingsShow == false ? true : false;
-                            });
-                          },
-                          style: ButtonStyle(
-                              overlayColor: MaterialStateProperty.all(secondaryColor)
-                          ),
-                          child: SvgPicture.asset('assets/icons/cog.svg', width: 20, height: 20, color: primaryColor),
-                        ),
-                        TextButton(
-                          onPressed: () {},
-                          style: ButtonStyle(
-                              overlayColor: MaterialStateProperty.all(secondaryColor)
-                          ),
-                          child: SvgPicture.asset('assets/icons/user.svg', width: 20, height: 20, color: primaryColor),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/login');
-                          },
-                          style: ButtonStyle(
-                              overlayColor: MaterialStateProperty.all(secondaryColor)
-                          ),
-                          child: SvgPicture.asset('assets/icons/sign-out.svg', width: 20, height: 20, color: primaryColor),
-                        ),
-                      ],
+        Container(
+            margin: EdgeInsets.all(0),
+            padding: EdgeInsets.fromLTRB(100, 15, 100, 15),
+            color: Color(0xB2000000),
+            child: Column(
+              children: [
+                // if (!Responsive.isDesktop(context))
+                //   IconButton(
+                //     icon: Icon(Icons.menu),
+                //     onPressed: context.read<MenuController>().controlMenu,
+                //   ),
+                // if (!Responsive.isMobile(context))
+                //   Spacer(flex: Responsive.isDesktop(context) ? 2 : 1),
+                // Expanded(child: SearchField()),
+                // ProfileCard()
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          overlayColor:
+                              MaterialStateProperty.all(secondaryColor)),
+                      child: SvgPicture.asset(
+                          'assets/icons/exclamation-square.svg',
+                          width: 20,
+                          height: 20,
+                          color: primaryColor),
                     ),
-                    SizedBox(height: 10,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        if (searchField == false)
-                          Row(
-                            children: [
-                              Image.asset('assets/images/logo_white.png'),
-                              Text('MATTERS', style: TextStyle(fontSize: 20, color: Colors.white),)
-      ],
-
-                          ),
-                        if (searchField == true)
-                          SizedBox(
-                            width: 700,
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                  labelText: 'Search',
-                                  labelStyle: TextStyle(color: primaryColor)
-                              ),
-              ),
-            ),
-                        TextButton(
-            onPressed: () {
-                            setState(() {
-                              searchField = searchField == false ? true : false;
-                            });
-            },
-                          style: ButtonStyle(
-                              overlayColor: MaterialStateProperty.all(secondaryColor)
-                          ),
-                          child: SvgPicture.asset('assets/icons/search.svg', width: 20, height: 20, color: primaryColor),
-                        ),
-        ],
-      ),
-
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          settingsShow = settingsShow == false ? true : false;
+                        });
+                      },
+                      style: ButtonStyle(
+                          overlayColor:
+                              MaterialStateProperty.all(secondaryColor)),
+                      child: SvgPicture.asset('assets/icons/cog.svg',
+                          width: 20, height: 20, color: primaryColor),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          overlayColor:
+                              MaterialStateProperty.all(secondaryColor)),
+                      child: SvgPicture.asset('assets/icons/user.svg',
+                          width: 20, height: 20, color: primaryColor),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/login');
+                      },
+                      style: ButtonStyle(
+                          overlayColor:
+                              MaterialStateProperty.all(secondaryColor)),
+                      child: SvgPicture.asset('assets/icons/sign-out.svg',
+                          width: 20, height: 20, color: primaryColor),
+                    ),
                   ],
-                )
-            ),
-            if(settingsShow == true)
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment:  CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 630,
-                    width: 260,
-                    child: Material(
-                      type: MaterialType.transparency,
-                      child: SideMenu(),
-                    )
-                  )
-                ],
-              )
-
-
-          ],
-        )
-
-  );
-}
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    if (searchField == false)
+                      Row(
+                        children: [
+                          Image.asset('assets/images/logo_white.png'),
+                          Text(
+                            'MATTERS',
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          )
+                        ],
+                      ),
+                    if (searchField == true)
+                      SizedBox(
+                        width: 700,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'Search',
+                              labelStyle: TextStyle(color: primaryColor)),
+                        ),
+                      ),
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          searchField = searchField == false ? true : false;
+                        });
+                      },
+                      style: ButtonStyle(
+                          overlayColor:
+                              MaterialStateProperty.all(secondaryColor)),
+                      child: SvgPicture.asset('assets/icons/search.svg',
+                          width: 20, height: 20, color: primaryColor),
+                    ),
+                  ],
+                ),
+              ],
+            )),
+        if (settingsShow == true)
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                  height: 630,
+                  width: 260,
+                  child: Material(
+                    type: MaterialType.transparency,
+                    child: SideMenu(),
+                  ))
+            ],
+          )
+      ],
+    ));
+  }
 }
