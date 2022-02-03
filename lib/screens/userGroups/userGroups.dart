ada016580bd128f6f82d4d5947a826d858dafd3f
Ivan Makarevich <i.makarevich@multiplex.ua>
2022-02-03 16:08:52 +0200

    Added class RowOfUserGroup

--- lib/screens/userGroups/userGroups.dart
+++ lib/screens/userGroups/userGroups.dart
@@ -1,10 +1,10 @@
+import 'package:admin/screens/userGroups/components/row_of_user_group.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:admin/constants/colors.dart';
 import 'package:admin/widgets/buttons.dart';
 import 'package:admin/widgets/containers.dart';
 import 'package:admin/widgets/textFields.dart';
 import 'package:flutter/material.dart';
-
 
 class UserGroupsPage extends StatefulWidget {
   @override
@@ -36,110 +36,75 @@ class _UserGroupsPage extends State<UserGroupsPage> with RestorationMixin {
         type: MaterialType.transparency,
         child: SafeArea(
             child: HeaderAndSideMenu(
-              widget: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+          widget: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'User Groups',
+                style: TextStyle(color: iconColor, fontSize: 20, fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Wrap(
+                alignment: WrapAlignment.spaceBetween,
                 children: [
-                  Text(
-                    'User Groups',
-                    style: TextStyle(
-                        color: iconColor,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600),
+                  Container(
+                    padding: EdgeInsets.only(bottom: 10),
+                    width: 300,
+                    child: OwnTextFieldWithIcons(
+                      labelText: 'Search',
+                      prefixIcon: FontAwesomeIcons.search,
+                    ),
                   ),
-                  SizedBox(height: 10,),
-                  Wrap(
-                    alignment: WrapAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.only(bottom: 10),
-                        width: 300,
-                        child: OwnTextFieldWithIcons(
-                          labelText: 'Search',
-                          prefixIcon: FontAwesomeIcons.search,
+                  Container(
+                    width: 300,
+                    child: Row(
+                      children: [
+                        Checkbox(
+                            value: archivedCheckBox.value,
+                            onChanged: (value) {
+                              setState(() {
+                                archivedCheckBox.value = value;
+                              });
+                            }),
+                        SizedBox(
+                          width: 5,
                         ),
-                      ),
-                      Container(
-                        width: 300,
-                        child: Row(
-                          children: [
-                            Checkbox(
-                                value: archivedCheckBox.value,
-                                onChanged: (value) {
-                                  setState(() {
-                                    archivedCheckBox.value = value;
-                                  });
-                                }
-                            ),
-                            SizedBox(
-                              width: 5,
-                            ),
-                            Text('Show Archived',
-                                style: TextStyle(
-                                  color: iconColor,
-                                  fontSize: 16,
-                                )
-                            ),
-                            OwnButtonICon(
-                              onPressed: () async {
-                                Navigator.pushNamed(
-                                  context,
-                                  '/editUserGroups',
-                                );
-                              },
-                              icon: Icons.add,
-                            ),
-                          ],
+                        Text('Show Archived',
+                            style: TextStyle(
+                              color: iconColor,
+                              fontSize: 16,
+                            )),
+                        OwnButtonICon(
+                          onPressed: () async {
+                            Navigator.pushNamed(
+                              context,
+                              '/editUserGroups',
+                            );
+                          },
+                          icon: Icons.add,
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
-                        child: Text('Name'),
-                      ),
-                      InkWell(
-                        hoverColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        onHover: (isHover) {
-                          setState(() {
-                            _color = _color == Colors.black ? secondaryColor : Colors.black;
-                          });
-                        },
-                        onTap: () {},
-                        child: Text('Administration',  style: TextStyle(color: _color, fontSize: 20)),
-                      ),
-                      InkWell(
-                        hoverColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        onHover: (isHover) {
-                          setState(() {
-                            _color = _color == Colors.black ? secondaryColor : Colors.black;
-                          });
-                        },
-                        onTap: () {},
-                        child: Text('Branding',  style: TextStyle(color: _color, fontSize: 20)),
-                      ),
-                      InkWell(
-                        hoverColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        onHover: (isHover) {
-                          setState(() {
-                            _color = _color == Colors.black ? secondaryColor : Colors.black;
-                          });
-                        },
-                        onTap: () {},
-                        child: Text('Demo',  style: TextStyle(color: _color, fontSize: 20)),
-                      ),
-                    ],
-                  )
                 ],
               ),
-            )
-        )
-    );
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                    child: Text('Name'),
+                  ),
+                  RowOfUserGroup(nameOfGroup: 'Administration'),
+                  RowOfUserGroup(nameOfGroup: 'Branding'),
+                  RowOfUserGroup(nameOfGroup: 'Demo'),
+                ],
+              )
+            ],
+          ),
+        )));
   }
 }
